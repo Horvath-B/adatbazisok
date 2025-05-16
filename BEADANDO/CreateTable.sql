3b47e0446745d4a39a74278aa64cbebd56bcc276
Horvath-B <bence.horvath5@stud.uni-corvinus.hu>
2025-05-16 19:43:27 +0200

    Adatbázis létrehozása
    Az adatbázist létrehozó sql parancsok

---
+++ BEADANDO/CreateTable.sql
@@ -0,0 +1,71 @@
+CREATE TABLE Vevo
+(
+    VevoID int PRIMARY KEY,
+    Nev varchar(20) NOT NULL,
+    Telefonszam varchar(20) NOT NULL,
+    Cim varchar(50) NOT NULL,
+    Maganszemely bit
+)
+
+CREATE TABLE Uzletkoto
+(
+    UzletkotoID int PRIMARY KEY,
+    Nev VARCHAR(20) NOT NULL,
+    Fizetes INT NOT NULL,
+    CONSTRAINT chkFizetes
+    CHECK(Fizetes > 0)
+)
+
+CREATE TABLE Meghajtas
+(
+    MeghajtasID int PRIMARY KEY,
+    Uzemanyag varchar(10) NOT NULL,
+    Fajta varchar(10) NOT NULL,
+    Tipus varchar(15),
+    Hengerurtartalom int,
+    Loero int NOT NULL,
+    CONSTRAINT chkLoero
+    CHECK(Loero > 0),
+    CONSTRAINT chkHengerurtartalom
+    CHECK(Hengerurtartalom > 0)
+)
+
+CREATE TABLE Autok
+(
+    AutoID int PRIMARY KEY,
+    Marka varchar(10) NOT NULL,
+    Tipus varchar(10) NOT NULL,
+    Felszereltseg varchar(15),
+    Meghajtas int,
+    Szin varchar(10),
+    Ar int NOT NULL,
+
+    CONSTRAINT chkAr
+    CHECK(Ar > 0),
+
+    CONSTRAINT fkMeghajtas
+    FOREIGN KEY (Meghajtas)
+    REFERENCES Meghajtas (MeghajtasID)
+)
+
+CREATE TABLE Vetel
+(
+    VetelID int PRIMARY KEY,
+    Vevo int,
+    Uzletkoto int,
+    Kocsi int,
+    Datum date NOT NULL,
+    FizetesiMod varchar(8) NOT NULL,
+
+    CONSTRAINT fkVevo
+    FOREIGN KEY (Vevo)
+    REFERENCES Vevo (VevoID),
+
+    CONSTRAINT fkUzletkoto
+    FOREIGN KEY (Uzletkoto)
+    REFERENCES Uzletkoto (UzletkotoID),
+
+    CONSTRAINT fkKocsi
+    FOREIGN KEY (Kocsi)
+    REFERENCES Autok (AutoID)
+)
