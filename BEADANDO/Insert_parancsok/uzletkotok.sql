18f0084eede34b2058ab0dd6dd03a135f33e2742
Horvath-B <bence.horvath5@stud.uni-corvinus.hu>
2025-05-18 18:54:32 +0200

    insertek

---
+++ BEADANDO/Insert_parancsok/uzletkotok.sql
@@ -0,0 +1,3 @@
+INSERT INTO Uzletkoto (uzletkotoid, nev, fizetes) VALUES (1, 'Balogh Júlia', 800000)
+INSERT INTO Uzletkoto (uzletkotoid, nev, fizetes) VALUES (2, 'Molnár Péter', 750000)
+INSERT INTO Uzletkoto (uzletkotoid, nev, fizetes) VALUES (3, 'Juhász Lajos', 820000)
